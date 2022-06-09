29c9fda9a95fa0dd13659fb3ad0ba1985ddd718a
CougarChan <gohan0136@gmail.com>
2022-06-09 09:02:02 -0400

    update on answers

---
+++ assets/js/questions.js
@@ -0,0 +1,57 @@
+var questions =[
+
+{
+title: "Commonly used data type DO NOT include:",
+choices: ["strings","booleans","numbers","alerts"],
+answer: "alerts"
+
+},
+
+{
+    title: "Which ones of these is among the three different types of error in JavaScripts?",
+    choices:["Animations time errors","Load time errors","Run time errors", "Logical Errors"],
+    answer: "Animation time errors"
+
+},
+
+{
+  title:"",
+  choices:["","","",""],
+  answers
+
+
+
+},
+{
+    title:"",
+    choices:["","","",""],
+    answers
+  
+  
+  
+  },
+  {
+    title:"",
+    choices:["","","",""],
+    answers
+  
+  
+  
+  }
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
