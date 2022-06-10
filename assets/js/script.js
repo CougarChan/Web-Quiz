80414aa047f8505082f4d4304e074d1a5c7a60d1
CougarChan <gohan0136@gmail.com>
2022-06-10 04:15:51 -0400

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,96 +1,149 @@
-var questions = document.querySelector("#questions");
-var timer  =document.querySelector ("#Timer")
-var choices =document.querySelector ("#choices")
-var initials =document.querySelector("#initials")
-var submit =document.querySelector  ("#submit")
-var time= questions.length *20;
-var timerTime;
 
-function startGame(){
-   var BeginningScreen = document.getElementById("start-screen")
-   BeginningScreen.setAttribute("class", "hide")
-   questions.removeAttribute("class");
 
-    timerTime = setInterval(clockTick, 1000);
-   timer.textContent = time;
- 
-   getQuestion();
+//Dom Set of rules
 
+var questionsTI = document.querySelector("#questions");
+var timerTI = document.querySelector("#time");
+var choicesTI = document.querySelector("#choices");
+var submitBtn = document.querySelector("#submit");
+var startBtn = document.querySelector("#start");
+var initialsTI= document.querySelector("#initials");
+var feedbackTI = document.querySelector("#feedback");
+
+
+// Var for question indexes
+var currentQuestionIndex = 0;
+var time = questions.length * 10;
+var timerId;
+
+// start of the function for quiz
+function startQuiz() {
+  var startScreenEl = document.getElementById("start-screen");
+  startScreenEl.setAttribute("class", "hide");
+
+  questionsTI.removeAttribute("class");
+
+
+  timerId = setInterval(clockTick, 1000);
+
+  timerTI.textContent = time;
+
+  getQuestion();
 }
+
+// This function is what retrieves the questions from its javascript file
 function getQuestion() {
-    // get current question object from array
-    var currentQuestion = questions[currentQuestionIndex];
-  
-    // update title with current question
-    var question_Ti = document.getElementById("question-title");
-    question_Ti.textContent = currentQuestion.title;
-  
-    // clear out any old question choices
-    choices.innerHTML = "";
-  
-    // loop over choices
-    currentQuestion.choices.forEach(function(choice, i) {
-      // create new button for each choice
-      var choiceBtn = document.createElement("button");
-      choiceBtn.setAttribute("class", "choice");
-      choiceBtn.setAttribute("value", choice);
-  
-      choiceBtn.textContent = i + 1 + ". " + choice;
-  
-      // attach click event listener to each choice
-      choiceBtn.onclick = questionClick;
-  
-      // display on the page
-      choices.appendChild(choiceBtn);
-    });
-  }
-  
 
+  var currentQuestion = questions[currentQuestionIndex];
+
+  // questions start
+  var titleEl = document.getElementById("question-title");
+  titleEl.textContent = currentQuestion.title;
+
+  choicesTI.innerHTML = "";
+
+  
+  currentQuestion.choices.forEach(function(choice, i) {
+    var choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "choice");
+    choiceNode.setAttribute("value", choice);
+
+    choiceNode.textContent = i + 1 + ". " + choice;
+
+     //Choice nodes
+    choiceNode.onclick = questionClick;
+    choicesTI.appendChild(choiceNode);
+  });
+}
+
+function questionClick() {
+  // wrong answer
+  if (this.value !== questions[currentQuestionIndex].answer) {
+    // subtract time
+    time -= 10;
+
+    if (time < 0) {
+      time = 0;
+    }
+    // show new time
+    timerTI.textContent = time;
+    feedbackTI.textContent = "Wrong! TRY HARDER";
+    feedbackTI.style.fontSize = "400%";
+  } else {
+    feedbackTI.textContent = "Correct!, GOODJOB KEEP GOING";
+    feedbackTI.style.fontSize = "800%";
+  }
+
+  feedbackTI.setAttribute("class", "feedback");
+  setTimeout(function() {
+    feedbackTI.setAttribute("class", "feedback hide");
+  }, 1000);
+
+  // next question
+  currentQuestionIndex++;
+  if (currentQuestionIndex === questions.length) {
+    quizEnd();
+  } else {
+    getQuestion();
+  }
+}
+// End of quiz
 function quizEnd() {
-    // stop timer
-    clearInterval(timerId);
-  
-    // show end screen
-    var endScreenEl = document.getElementById("finished-screen");
-    endScreenEl.removeAttribute("class");
-  
-    // show final score
-    var finalScoreEl = document.getElementById("score");
-    finalScoreEl.textContent = time;
-  
-    // hide questions section
-    questionsEl.setAttribute("class", "hide");
-  }
+  clearInterval(timerId);
 
+  
+  var endScreenEl = document.getElementById("end-screen");
+  endScreenEl.removeAttribute("class");
+  var finalScoreEl = document.getElementById("final-score");
+  finalScoreEl.textContent = time;
+
+  questionsTI.setAttribute("class", "hide");
+}
 
 function clockTick() {
-    time--;
 
-    timer.textContent = time;
-    if (time <= 0) {
-      quizEnd();
-    }
+
+  time--;
+  timerTI.textContent = time;
+// time resets
+  if (time <= 0) {
+    quizEnd();
   }
-  function saveHighscore() {
-    // get value of input box
-    var initials = initialsEl.value.trim();
-  
-    if (initials !== "") {
-      // get saved scores from localstorage, or if not any, set to empty array
-      var highscores =
-        JSON.parse(window.localStorage.getItem("highscores")) || [];
-  
-      // format new score object for current user
-      var newScore = {
-        score: time,
-        initials: initials
-      };
-  
-      // save to localstorage
-      highscores.push(newScore);
-      window.localStorage.setItem("highscores", JSON.stringify(highscores));
-  
-      // redirect to next page
-      window.location.href = "highscore.html";
-    }
+}
+ 
+
+// highscore function
+function saveHighscore() {
+  var initials = initialsEl.value.trim();
+
+  if (initials !== "") {
+    
+    var highscores =
+      JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+    
+    var newScore = {
+      score: time,
+      initials: initials
+    };
+
+  // saves it all to local storage
+
+    highscores.push(newScore);
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+    window.location.href = "highscore.html";
   }
+}
+
+function checkForEnter(event) {
+  if (event.key === "Enter") {
+    saveHighscore();
+  }
+}
+
+
+// buttons that begin the whole process
+
+submitBtn.onclick = saveHighscore;
+startBtn.onclick = startQuiz;
+initialsEl.onkeyup = checkForEnter;
